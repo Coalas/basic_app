cf6e9ea4b8579c29e6f9528e55d47683dbfc4c02
coalas <coalas.bladeforums@gmail.com>
2011-05-30 20:57:42 +0200

    v0.1

--- src/test/java/pl/studioit/puzzlecrm/test/JCGTest.java
+++ src/test/java/pl/studioit/puzzlecrm/test/JCGTest.java
@@ -1,5 +1,8 @@
 package pl.studioit.puzzlecrm.test;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import junit.framework.TestCase;
 
 import org.junit.Test;
@@ -8,10 +11,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import pl.studioit.puzzlecrm.domain.Category;
 import pl.studioit.puzzlecrm.domain.MyEntity;
 import pl.studioit.puzzlecrm.drools.MyDomain;
 import pl.studioit.puzzlecrm.jms.MyQueueSender;
 import pl.studioit.puzzlecrm.service.ActivitiService;
+import pl.studioit.puzzlecrm.service.CategoryService;
 import pl.studioit.puzzlecrm.service.DroolsService;
 import pl.studioit.puzzlecrm.service.MyEntityService;
 
@@ -20,7 +25,8 @@ import pl.studioit.puzzlecrm.service.MyEntityService;
 public class JCGTest extends TestCase {
 	@Autowired
 	private MyEntityService myEntityService;
-	
+	@Autowired
+	private CategoryService categoryService;
 	@Autowired
 	private ActivitiService activitiService;
 	
@@ -35,9 +41,26 @@ public class JCGTest extends TestCase {
 		MyEntity entity = new MyEntity();
 		entity.setName("test");
 		myEntityService.persist(entity);
+	}
+	@Test
+	public void testCategoryEntity() {
+		//categoryService.findLevel(0);
+		Category entity = new Category();
+		entity.setName("testowa");
+		
+		//List<Category> list = new  LinkedList<Category>();
+		//list.add(entity);
+		//categoryService.find(3L).setChildren(list);
+		
+		Category cat = categoryService.find(3L);
+		assertEquals(3, cat.getChildren().size());
+		entity.setParent(cat);
+		//cat.getChildren().add(entity);
+		//categoryService.merge(cat);
+		categoryService.persist(entity);
+		
 
 	}
-	
 	@Test
 	public void testActiviti() {
 		activitiService.startProcess();
