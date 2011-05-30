5e7769aec3448d9bd081b770ee6cc61b0331a90e
coalas <coalas.bladeforums@gmail.com>
2011-05-30 20:59:05 +0200

    v0.1

---
+++ src/main/java/pl/studioit/puzzlecrm/service/CategoryService.java
@@ -0,0 +1,20 @@
+package pl.studioit.puzzlecrm.service;
+
+
+import java.util.List;
+
+import pl.studioit.puzzlecrm.domain.Category;
+
+public interface CategoryService {
+	public void persist(Category entity);
+	public void remove(Category entity);
+	public void removeAll();
+	public void refresh(Category entity);
+	public Category find(Long id);
+	public void flush();
+	public void merge(Category entity);
+	public long count();
+	public List<Category> findAll();
+	public List<Category> findEntries(int firstResult, int maxResults);
+	public List<Category> findLevel(int level);
+}
